d430854d40f01683ebe329ecc9182f66896fe431
jaclyn16 <jgrix16@yahoo.com>
2026-03-02 13:57:37 -0800

    added code for pizza-order-printer.js

--- birthstone-finder/lecture-birthstone-finder.js
+++ birthstone-finder/lecture-birthstone-finder.js
@@ -3,3 +3,26 @@
 ----------------------------
 We will refactor the Birthstone Finder app to read birthstone data from a separate JSON file.
 ------------------------- */
+
+// ES Module way
+import fs from "fs/promises";
+// Common JS way
+// const fs = require("fs/promises");
+
+// user input
+const month = process.argv[2];
+
+// Read birthstone data from data.json and get the birthstone for each month
+async function getBirthStone() {
+    // The readfile method is part of the fs object
+    // It takes in 2 parameters:
+    // 1. the file we want to read (relative file path)
+    // 2. the way the file is encoded
+    const data = await fs.readFile("data.json", "utf8");
+    const birthstones = JSON.parse(data)
+
+    birthstones[month]
+        ? console.log(`The birthstone for ${month} is ${birthstones[month]}`)
+        : console.log("Invalid month, Please enter a valid month name, and make sure it is capitolized.")
+}
+getBirthStone()
--- pizza-order-printer/pizza-order-printer.js
+++ pizza-order-printer/pizza-order-printer.js
@@ -5,3 +5,27 @@ Make a Node.js app that prints pizza orders to the console.
 
 Check the README.md file for instructions.
 ------------------------- */
+import fs from "fs/promises";
+
+const userInput = process.argv[2];
+const orderNumber = process.argv[3];
+
+async function pizzaApp() {
+    const file = await fs.readFile("data.json", "utf8");
+    const orders = JSON.parse(file);
+
+    if (userInput === "getAllOrders") {
+        for (let i = 0; i < orders.length; i++) {
+            console.log(orders[i]);
+        }
+    }
+    if (userInput === "getOneOrder") {
+        if (orders[orderNumber] !== undefined) {
+            console.log(orders[orderNumber]);
+        } else {
+            console.log("Order not Found. Please enter a valid order number.");
+        }
+    }
+}
+
+pizzaApp();
